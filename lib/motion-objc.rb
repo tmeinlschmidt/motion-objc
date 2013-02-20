c7fd44faf09c4c59e35d1da9187e68321a7cef9e
Tom Meinlschmidt <tomas@meinlschmidt.com>
2013-02-20 17:36:32 +0100

    added pathname as require

--- lib/motion-objc.rb
+++ lib/motion-objc.rb
@@ -1,5 +1,6 @@
 require "motion/project/objc/version"
 require "fileutils"
+require "pathname"
 
 unless defined?(Motion::Project::Config)
   raise "This file must be required within a RubyMotion project Rakefile."
